fb25e5085c54420fd0915c9730e99a19696974d2
Daniel Manoel <danielmanoelfilho44@gmail.com>
2025-10-28 08:31:42 -0300

    ✨ feat: initialize database setup with drizzle-orm and create webhooks table

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,46 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import {fastifySwagger} from '@fastify/swagger'
+import {fastifyCors} from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
+import {env} from './env'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running')
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('🔥 HTTP server running on http://localhost:3333')
+  console.log('📚 Docs available at http://localhost:3333/docs')
 })
