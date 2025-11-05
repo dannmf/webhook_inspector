4064d344cdfda60bd5832676ac173b4c68b5ec24
Daniel Manoel <danielmanoelfilho44@gmail.com>
2025-11-05 17:22:12 -0300

    ✨feat: create request content page

---
+++ web/src/components/section-title.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+interface SectionTitleProps extends ComponentProps<'h3'> {}
+
+
+export function SectionTile({ className, ...props } : SectionTitleProps){
+  return(
+    <h3 className={
+      twMerge(
+        'text-base font-semibold text-zinc-100', className
+      )
+    } {...props} />
+
+  )
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
 import { Sidebar } from '../components/sidebar'
+import { WebhookDetailHeader } from '../components/webhook-detail-header'
+import { SectionTile } from '../components/section-title'
+import { SectionDataTable } from '../components/section-data-table'
+import { CodeBlock } from '../components/ui/code-block'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const overviewData = [
+    {key: 'Method', value: 'POST'},
+    {key: 'URL', value: '/webhook'},
+    {key: 'Headers', value: 'Content-Type: application/json'},
+    {key: 'Body', value: '{"message": "Hello World"}'}
+  ]
   return (
     <div className="h-screen bg-zinc-900">
       <PanelGroup direction="horizontal">
@@ -17,7 +27,31 @@ function Index() {
         <PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150" />
 
         <Panel defaultSize={80} minSize={40}>
-          conteudo
+          <div className='flex h-full flex-col'>
+                <WebhookDetailHeader />
+            <div className='flex-1 overflow-y-auto'>
+              <div className='space-y-6 p-6'>
+                <div className='space-y-4'>
+                  <SectionTile>Request Overview</SectionTile>
+                <SectionDataTable data={overviewData} />
+                </div>
+                <div className='space-y-4'>
+                  <SectionTile>Query Parameters</SectionTile>
+                <SectionDataTable data={overviewData} />
+                </div>
+                <div className='space-y-4'>
+                  <SectionTile>Headers</SectionTile>
+                <SectionDataTable data={overviewData} />
+                </div>
+                <div className='space-y-4'>
+                  <SectionTile>Request Body</SectionTile>
+                <CodeBlock code={JSON.stringify(overviewData,null,2)} />
+                </div>
+                <div/>
+              </div>
+            </div>
+          </div>
+
         </Panel>
       </PanelGroup>
     </div>
