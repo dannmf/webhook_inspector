91ae93c284f7539e7f10bdbb2bcdb3be656a5706
Daniel Manoel <danielmanoelfilho44@gmail.com>
2025-11-06 14:05:09 -0300

    ✨new route: get webhook

---
+++ api/src/routes/get-webhook.ts
@@ -0,0 +1,38 @@
+import type { FastifyPluginAsyncZod} from 'fastify-type-provider-zod'
+import { z } from 'zod';
+import { createSelectSchema} from 'drizzle-zod'
+import { webhooks } from '@/db/schema'
+import { db } from '@/db';
+import { eq } from 'drizzle-orm';
+
+export const getWebhook: FastifyPluginAsyncZod = async (app) => {
+    app.get('/api/webhooks/:id',
+         {
+            schema: {
+                summary: 'Get a specific webhook by ID',
+                tags: ['Webhooks'],
+                querystring: z.object({
+                    id: z.uuidv7(),
+                }),
+                response: {
+                    200: createSelectSchema(webhooks),
+                    400:  z.object({message: z.string()}),
+
+                },
+            },
+        },
+        async (request, reply) => {
+            const { id } = request.query
+            const result = await db
+              .select()
+              .from(webhooks)
+              .where(eq(webhooks.id, id))
+              .limit(1)
+
+            if(result.length === 0){
+              return reply.status(400).send({message: 'Webhook not found'})
+            }
+
+            return reply.send(result[0])
+        },
+    )}
